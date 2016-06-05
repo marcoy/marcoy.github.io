6d5693a7f56f5d46df3b9aebbd7c0a843c58a667
Marco Yuen <marcoy@gmail.com>
2016-06-04 20:16:30 -0400

    travis script cleanup

--- build.sh
+++ build.sh
@@ -13,20 +13,15 @@ set -e
 # cleanup
 rm -rf "$BUILD_OUTPUT"
 
-#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
 git clone -b jekyll-staging https://${GH_TOKEN}@github.com/marcoy/marcoy.github.io.git "$BUILD_OUTPUT"
 
+# Clear out the content of ${BUILD_OUTPUT}
 rm -rf "${BUILD_OUTPUT}/*"
 
-# copy generated HTML site to `master' branch
-# cp -R _site/* ../marcoy.github.io.master
-
-# build site with `jekyll', by default to `_site' folder
 jekyll build -V -d "$BUILD_OUTPUT"
 
-# commit and push generated content to `master' branch
-# since repository was cloned in write mode with token auth - we can push there
 cd "$BUILD_OUTPUT"
+git status
 git add -A .
 git config user.email "marcoy@gmail.com"
 git config user.name "Marco Yuen"
